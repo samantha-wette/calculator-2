2b1784138a46591013a749602ab7e882a9483bec
Samantha Wette <samantha.wette@gmail.com>
2022-03-30 14:54:12 -0700

    Pseudocode added

--- calculator.py
+++ calculator.py
@@ -4,4 +4,31 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+user_equation = input("Please enter your equation")
+
+
+#tokenize input
+
+#go through the first value in the list, check with a conditional 
+# to know which function to use
+#if the first token is q, then we quit
+
+#call that function with list item 2-3 as arguments
+#return the value that comes from running the function
+
+
+#add
+
+#subtract
+
+#multiply
+
+#divide
+
+#square
+
+#cube
+
+#power
+
+#mod
